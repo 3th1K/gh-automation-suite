f64019a7f1a00e209d86ece96717a7c0fce40f77
Mehedi Rahaman <mehedirahaman28@gmail.com>
2024-12-06 22:46:22 +0530

    Cleaned up for GitHub Pages deployment

--- Src/Core/Application/Interfaces/IGitHubAutomationService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.Interfaces;
-
-public interface IGitHubAutomationService
-{
-    void Configure(string userName, string userApiToken, string repositoryName);
-
-    Task AutomateGitHubContributionAsync();
-}
--- Src/Core/Domain/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace Domain.Models;
-
-public class User
-{
-    [JsonProperty("login")]
-    public string Login { get; init; } = null!;
-}
